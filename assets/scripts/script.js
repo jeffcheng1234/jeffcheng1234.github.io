39be24044f8bdb92f4e3587692a955f5842f0329
Jeffery <jeffcheng1234@berkeley.edu>
2020-12-02 23:12:43 +0800

    finished

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,17 +1,12 @@
 
 function setFrame(expClass) {
-    console.log(expClass);
     let frames = document.getElementsByClassName('experience--container');
-    console.log(frames)
     let descriptions = document.getElementsByClassName('experience--body');
-    console.log(descriptions)
     for (let i = 0; i < frames.length; i++) {
         if (frames[i].classList.contains(expClass)) {
             frames[i].classList.remove('hidden');
-            console.log(`added ${frames[i]}`)
         } else {
             frames[i].classList.add('hidden');
-            console.log(`removed ${frames[i]}`)
         }
         if (descriptions[i].classList.contains(expClass)) {
             descriptions[i].classList.remove('hidden');
@@ -21,20 +16,39 @@ function setFrame(expClass) {
     }
 }
 
+function showClicked(buttonID) {
+    let buttons = document.getElementsByClassName('experience--button');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].firstElementChild.style.backgroundColor = '#EA9F4B';
+    }
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].children[1].style.fontWeight = 400;
+    }
+    let pressed = document.getElementById(buttonID);
+    pressed.firstElementChild.style.backgroundColor = '#6B461B';
+    pressed.children[1].style.fontWeight = "bold";
+
+    
+}
 function study_cat(){
     setFrame('exp--study_cat');
+    showClicked('button--study_cat');
 }
 function csm(){
     setFrame('exp--csm');
+    showClicked('button--csm');
 }
 function project_taiwan(){
     setFrame('exp--project_taiwan');
+    showClicked('button--project_taiwan');
 }
 function lab_assistant(){
     setFrame('exp--lab_assistant');
+    showClicked('button--lab_assistant');
 }
 function tsa(){
     setFrame('exp--tsa');
+    showClicked('button--tsa');
 }
 document.getElementById('button--study_cat').onclick = study_cat;
 document.getElementById('button--csm').onclick = csm;
